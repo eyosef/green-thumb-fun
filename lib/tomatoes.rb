0e3255bbceec8ce61d811be78e16c302b392814a
Eden Mesfun <emesfun@gmail.com>
2018-04-17 19:51:50 +0000

    using Nokogiri to open a site to test web scraping

--- lib/tomatoes.rb
+++ lib/tomatoes.rb
@@ -1,11 +1,13 @@
 require 'nokogiri'
+require 'open-uri'
 require 'HTTParty'
 require 'pry'
 
 class Tomatoes
 
   def self.tomatoes
-    binding.pry
+    doc = Nokogiri::HTML(open("https://www.seedsavers.org/category/tomato"))
+
     @@seedsavers = HTTParty.get("https://www.seedsavers.org/category/tomato")
     @@tomatoes = Nokogiri::HTML(@@seedsavers)
 
