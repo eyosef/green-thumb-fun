da27899f05f4a1b6808deae56b4fddcd8447171a
Eden Mesfun <emesfun@gmail.com>
2018-04-17 21:33:15 +0000

    updating scraper to scrape another website

--- lib/tomatoes.rb
+++ lib/tomatoes.rb
@@ -1,17 +1,20 @@
 require 'nokogiri'
 require 'open-uri'
-require 'HTTParty'
+require 'httparty'
 require 'pry'
 
 class Tomatoes
 
-  def tomato_scraper
-    doc = Nokogiri::HTML(open("https://www.swallowtailgardenseeds.com/veggies/heirloom.html"))
-    doc.css(".https://www.swallowtailgardenseeds.com/veggies/heirloom.html").each do |tomato|
+  def scraper1
+    # doc = Nokogiri::HTML(open("https://www.swallowtailgardenseeds.com/veggies/heirloom.html"))
+    # doc.css(".https://www.swallowtailgardenseeds.com/veggies/heirloom.html").each do |tomato|
+    # end
+    @@seedsavers = HTTParty.get("https://www.seedsavers.org/amish-paste-tomato")
+    @@tomatoes = Nokogiri::HTML(@@seedsavers)
+
+    @@tomatoes.css(".item-details").each do |tomato|
       binding.pry
     end
-    # @@seedsavers = HTTParty.get("https://www.seedsavers.org/category/tomato")
-    # @@tomatoes = Nokogiri::HTML(@@seedsavers)
 
   end
 
