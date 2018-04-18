4c5e4c2fc9437f4806841082e851779e6ed958f5
Eden Mesfun <emesfun@gmail.com>
2018-04-18 16:39:08 +0000

    refactoring web scraping method and creating instance variables to capture data

--- lib/tomatoes.rb
+++ lib/tomatoes.rb
@@ -9,13 +9,40 @@ class Tomatoes
     # doc = Nokogiri::HTML(open("https://www.swallowtailgardenseeds.com/veggies/heirloom.html"))
     # doc.css(".https://www.swallowtailgardenseeds.com/veggies/heirloom.html").each do |tomato|
     # end
-    @@seedsavers = HTTParty.get("https://www.seedsavers.org/amish-paste-tomato")
+    @@seedsavers = HTTParty.get("https://www.fedcoseeds.com/seeds/?cat=Tomatoes")
     @@tomatoes = Nokogiri::HTML(@@seedsavers)
 
-    @@tomatoes.css(".item-characteristics").each do |tomato|
-      binding.pry
-    end
+      @@tomatoes.css(".search-results-text").each do |tomato|
 
-  end
+          @details = []
+
+            variety = tomato.css("a").children[0].text #for db
+            content = tomato.css(".description").children.text
+
+            split_words = content.split(" ")
+            # days = split_words[0..1].join(" ") #for db
+
+            split_content = content.split(".")
+            split_content.delete("")
+            split_content.delete("        read more")
+
+            split_content.each do |string|
+                if string == " Indeterminate" || string == " Determinate"
+                    @growth_type = string #for db
+                    #making variable instance variable to call on it outside of the each iterator
+                elsif !string.include?("days")
+                  @details << string
+                elsif string.include?("days")
+                  array = []
+                  split_string = string.split(" ")
+                  days = split_string[0..1].join(" ")
+                  @days = days
+                  binding.pry
+                end #if statement
+            end #each iteration, split_content
+
+
+      end #each iterator, @@tomatoes
+  end #scraper method
 
 end #Tomatoes class
