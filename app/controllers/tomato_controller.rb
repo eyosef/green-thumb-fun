71154e103010f385de9dc8f5957221ad3b728467
Eden Mesfun <emesfun@gmail.com>
2018-04-19 18:34:30 +0000

    Done.

--- app/controllers/tomato_controller.rb
+++ app/controllers/tomato_controller.rb
@@ -1,7 +1,14 @@
 class TomatoController < ApplicationController
 
-  get '/all_plants' do
+  get '/all_tomatoes' do
     erb :'tomatoes/show_tomatoes'
+  end
+
+  get '/new_variety' do
+    erb :'tomatoes/new_variety'
+  end
+
+  post '/new_variety' do
   end
 
 end
