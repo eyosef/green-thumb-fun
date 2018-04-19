1613689c11be298e3c2e02102374cc3f0def5b3a
Eden Mesfun <emesfun@gmail.com>
2018-04-19 19:39:28 +0000

    debugging GET route to edit submission

--- app/controllers/tomato_controller.rb
+++ app/controllers/tomato_controller.rb
@@ -1,14 +1,32 @@
 class TomatoController < ApplicationController
 
-  get '/all_tomatoes' do
+  get '/tomatoes/all' do
     erb :'tomatoes/show_tomatoes'
   end
 
-  get '/new_variety' do
+  post '/tomatoes/new' do
+    @tomato = Tomato.new
+    @tomato.variety = params["variety"]
+    @tomato.growth_type = params["growth_type"]
+    @tomato.days = params["days"]
+    @tomato.details = params["details"]
+    @tomato.save
+
+    @user = User.find_by_id(session["user_id"])
+    @tomato.id = @user.tomato_id
+    @user.tomato_id = @tomato.id
+
+    erb :'tomatoes/show_tomato'
+  end
+
+  get '/tomatoes/new' do
     erb :'tomatoes/new_variety'
   end
 
-  post '/new_variety' do
+  get '/tomatoes/:id/edit' do
+    binding.pry
+    @tomato = Tomato.find_by(params["id"].to_i)
+    @user = User.find_by(session["user_id"])
   end
 
 end
