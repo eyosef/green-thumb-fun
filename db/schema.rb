87512153bc5603a6ef21c0af7aaf8a5de30d43d1
Eden Mesfun <emesfun@gmail.com>
2018-04-19 15:14:32 +0000

    changed user column password to password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180418195947) do
+ActiveRecord::Schema.define(version: 20180419132920) do
 
   create_table "tomatoes", force: :cascade do |t|
     t.string "variety"
@@ -22,8 +22,9 @@ ActiveRecord::Schema.define(version: 20180418195947) do
 
   create_table "users", force: :cascade do |t|
     t.string  "username"
-    t.string  "password"
+    t.string  "password_digest"
     t.integer "tomato_id"
+    t.string  "email"
   end
 
 end
