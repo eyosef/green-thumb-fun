5e0ed140c3341b1d3adf37132940801e774e3d55
Eden Mesfun <emesfun@gmail.com>
2018-04-19 15:12:25 +0000

    created helper methods to verify log in/out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,23 @@
 require './config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions unless test?
+    set :session_secret, "secret"
+  end
+
+  helpers do
+    def logged_in
+      !!current_user
+    end
+
+    def current_user
+      session.include?("user_id")
+    end
   end
 
   get '/' do
