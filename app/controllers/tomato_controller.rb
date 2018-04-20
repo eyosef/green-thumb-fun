92ff0e68c5b55f3dccd99833dff045acb2dc752b
Eden Mesfun <emesfun@gmail.com>
2018-04-20 16:48:28 +0000

    refactored code in multiple routes to edit individual submissions, create URLs with ids, edit individual submissions for updating, etc.

--- app/controllers/tomato_controller.rb
+++ app/controllers/tomato_controller.rb
@@ -3,36 +3,61 @@ class TomatoController < ApplicationController
   get '/tomatoes/all' do
     @all_tomatoes = Tomato.all
 
-    erb :'tomatoes/show_tomatoes'
+    if logged_in
+      erb :'tomatoes/show_tomatoes'
+    else
+      redirect to "/login"
+    end
   end
 
-  post '/tomatoes/new' do
+  post '/tomatoes' do
     @tomato = Tomato.new
     @tomato.variety = params["variety"]
-    @tomato.growth_type = params["growth_type"]
+    @tomato.growth_type = params["indeterminate"] || params["determinate"]
     @tomato.days = params["days"]
     @tomato.details = params["details"]
     @tomato.save
 
     @user = User.find_by_id(session["user_id"])
+    @tomato.user_id = @user.id
+    @tomato.save
 
-    erb :'tomatoes/show_tomato'
+    redirect to "/tomatoes/#{@tomato.id}"
   end
 
-  get '/tomatoes/new' do
+  get '/tomatoes' do
     erb :'tomatoes/new_variety'
   end
 
   get '/tomatoes/:id/edit' do
 
-    @tomato = Tomato.find_by_id(params["id"].to_i)
-    @user = User.find_by_id(session["user_id"])
+    @tomato = Tomato.find_by_id(params["id"])
+    @user = User.find_by_id(@tomato.user_id)
 
     erb :'tomatoes/edit_tomato'
   end
 
   post '/tomatoes/:id' do
-    binding.pry
+
+    @tomato = Tomato.find_by_id(params["id"])
+    @user = User.find_by_id(@tomato.user_id)
+
+    @tomato.variety = params["variety"]
+    @tomato.growth_type = params["indeterminate"] || params["determinate"]
+    @tomato.days = params["days"]
+    @tomato.details = params["details"]
+    @tomato.save
+    @tomato.user_id = @user.id
+    @tomato.save
+
+    redirect to "/tomatoes/#{@tomato.id}"
+  end
+
+  get '/tomatoes/:id' do
+    @tomato = Tomato.find_by_id(params["id"])
+    @user = User.find_by_id(@tomato.user_id)
+
+    erb :'tomatoes/show_tomato'
   end
 
 end
