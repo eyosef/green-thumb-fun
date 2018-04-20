69ce858d76e1071dcc0bde2475ecf1ed7e44df5f
Eden Mesfun <emesfun@gmail.com>
2018-04-20 14:20:17 +0000

    included #slug and .find_by_slug methods for slugifying URLs

--- app/models/tomato.rb
+++ app/models/tomato.rb
@@ -1,4 +1,17 @@
 class Tomato < ActiveRecord::Base
   belongs_to :users
 
+  def slug
+    self.variety.split(" ").join("-").downcase
+  end
+
+  def self.find_by_slug(slug)
+    all_tomatoes = Tomato.all
+    tomato = all_tomatoes.find do |tomato|
+              tomato.slug == slug
+            end
+
+    return tomato
+  end
+
 end #Tomatoes class
