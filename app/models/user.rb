2de3d1056600dc8e4bad4e9651853776cb3a6d6a
Eden Mesfun <emesfun@gmail.com>
2018-04-20 14:20:46 +0000

    included #slug and .find_by_slug to slugify URLs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ActiveRecord::Base
   has_many :tomatoes
   has_secure_password
 
+  def slug
+    self.username.split(" ").join("-").downcase
+  end
+
+  def self.find_by_slug(slug)
+    all_users = User.all
+    user = all_users.find do |user|
+              user.slug == slug
+            end
+
+    return user
+  end
+
 end
